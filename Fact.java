31a17effa6d2c90b77894ea0a988dbb4189b2e4f
PankajKumarC <39941015+PankajKumarC@users.noreply.github.com>
2018-06-05 11:55:46 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fact
+{
+		public static void main(String[] args)
+		{
+
+			int n,i,f = 1;
+			Scanner s = new Scanner(System.in);
+			n=s.nextInt();
+			for(i = 1;i <= n;i++)
+			{
+				f*=i;
+			}
+			System.out.println(f);			
+		}
+
+}
