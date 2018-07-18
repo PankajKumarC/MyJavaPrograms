5bde0c8c3984f7dd9738d833aad7993d53dba3dc
PankajKumarC <39941015+PankajKumarC@users.noreply.github.com>
2018-07-18 21:08:25 +0530

    Create Main.java

---
+++ Palindrome/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+	
+		int n, i, r = 0, s = 0, temp;
+		Scanner x = new Scanner(System.in);
+		n = x.nextInt();
+		temp = n;
+
+
+		while(n != 0) {
+			r = n%10;
+			s = (s * 10) + r;
+			n /= 10;		
+		}
+
+
+		if(s == temp)
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not Palindrome");
+	}
+}
+		
